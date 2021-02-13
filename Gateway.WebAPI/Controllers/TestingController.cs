1908fb60ca080760348db46ca8f4721cbb1b1b73
Elias Smith <goodanonim69@gmail.com>
2021-02-13 17:32:21 +0200

    Initial commit

---
+++ Gateway.WebAPI/Controllers/TestingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Playground.Controllers
+{
+    [ApiController]
+    [Route("testing")]
+    public class TestingController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestingController> _logger;
+
+        public TestingController(ILogger<TestingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { name = "Yolo"});
+        }
+    }
+}
